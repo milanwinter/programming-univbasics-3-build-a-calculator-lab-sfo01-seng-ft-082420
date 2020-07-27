2c3ee4093ba9961a09e85bb37a9c5df48de709c8
Milan Winter <milanwinter8@gmail.com>
2020-07-27 22:10:40 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,26 +1,21 @@
 def addition(num1, num2)
   sum = num1 + num2
-  sum
 end
 
 def subtraction(num1, num2)
   subtract = num1 - num2
-  subtract
 end
 
 def division(num1, num2)
   divide = num1 / num2
-  divide
 end
 
 def multiplication(num1, num2)
   multiply = num1 * num2
-  multiply
 end
 
 def modulo(num1, num2)
  modul = num1 % num2
- modul
 end
 
 def square_root(num)
